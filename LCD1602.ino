c60be664d836e2921ef2790eb281a1f4d1162249
Xin Zhang <zju.zhangxin@gmail.com>
2015-04-15 03:58:50 +0800

    Create LCD1602.ino

---
+++ LCD1602.ino
@@ -0,0 +1,12 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+void setup() {
+  lcd.begin(16, 2);
+  lcd.print("hello, world!");
+}
+
+void loop() {
+  lcd.setCursor(0, 1);
+  lcd.print(millis()/1000);
+}
